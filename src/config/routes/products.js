3bc0f11f5342b260f3ccc2029cab144e479e8ffe
thiagometal <thiagometal.fne@gmail.com>
2018-06-13 18:37:52 -0400

    add método post de produtos

--- src/config/routes/products.js
+++ src/config/routes/products.js
@@ -6,8 +6,34 @@ router.get('/', function (req, res, next){
     var db = require('../database');
     var schema = require('../../products/schema');
     var Products = db.Mongoose.model('products', schema.ProductSchema);
+    
     Products.find({}).lean().exec(function(e, docs) {
         res.json(docs);
+        res.end();
+    });
+});
+
+/*POST ONE product. */
+router.post('/', function(req, res, next){
+    var db = require('../database');
+    var schema = require('../../products/schema');
+    var Product = db.Mongoose.model('products', schema.ProductSchema);
+    console.log(req.body.productInfo[0]['paymentConditions']);
+    console.log(req.body.productInfo[0].paymentConditions);
+    var newProduct = new Product({businessId: req.body.businessId, name: req.body.name, 
+                                    imageName: req.body.imageName, detailUrl: req.body.detailUrl,
+                                    price: req.body.price, oldPrice: req.body.oldPrice,
+                                    category: req.body.category,
+                                    productInfo: req.body.productInfo[0]
+                                });
+    
+    newProduct.save(function(err) {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            res.end();
+            return;
+        }
+        res.json(newProduct);
         res.end();
     });
 });
