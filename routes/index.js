374c602f401d533afb409a2c00f49d43cfde9905
thiagometal <thiagometal.fne@gmail.com>
2018-06-13 13:26:18 -0400

    add rota de listagem de todos os produtos

--- routes/index.js
+++ routes/index.js
@@ -7,3 +7,13 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+
+/*GET all products. */
+router.get('/products', function (req, res, next){
+    var db = require('../database');
+    var Products = db.Mongoose.model('products', db.ProductSchema);
+    Products.find({}).lean().exec(function(e, docs) {
+        res.json(docs);
+        res.end();
+    });
+});
